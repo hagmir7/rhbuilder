6be79dbc22b3b1ecd9cf81aaccf2b38d74fefe0a
Agmir Hassane <hagmir6@gmail.com>
2025-07-15 17:04:32 +0100

    Add new envirements

--- routes/api.php
+++ routes/api.php
@@ -12,11 +12,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->group(function(){
 
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
-    Route::post('/login', [AuthController::class, 'login']);
+    
 
     Route::post('/register', [AuthController::class, 'register']);
 
