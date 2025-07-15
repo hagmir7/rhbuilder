c131733d8c471554dfa98286c048f835371f6fab
Agmir Hassane <hagmir6@gmail.com>
2025-07-15 15:41:00 +0100

    Add new envirements

--- database/migrations/2025_07_14_155551_create_needs_table.php
+++ database/migrations/2025_07_14_155551_create_needs_table.php
@@ -13,18 +13,31 @@ return new class extends Migration
     {
         Schema::create('needs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('service_id')->constrained()->onDelete('cascade');
-            $table->foreignId('responsable_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('diplome_id')->constrained()->onDelete('cascade');
+
+            $table->foreignId('service_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->foreignId('responsable_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+
+            $table->foreignId('diploma_id')
+                ->constrained()
+                ->onDelete('cascade');
+
             $table->integer('experience_min')->default(0);
-            $table->integer('gender')->nullable(); // 0 = homme, 1 = femme, 2 = indifférent par ex.
-            $table->integer('min_age')->default(20);
-            $table->integer('max_age')->default(30);
-            $table->integer('status')->default(1); // ex: 1 = actif, 0 = inactif
+            $table->unsignedTinyInteger('gender')->nullable(); // 0: Homme, 1: Femme, etc.
+            $table->unsignedTinyInteger('min_age')->default(20);
+            $table->unsignedTinyInteger('max_age')->default(30);
+            $table->unsignedTinyInteger('status')->default(1); // 1: actif, 0: inactif...
+
             $table->string('description')->nullable();
+
             $table->timestamps();
         });
     }
+
 
 
     /**
--- database/migrations/2025_07_15_112733_create_interviews_table.php
+++ database/migrations/2025_07_15_112733_create_interviews_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
             $table->id();
             $table->string('code')->unique();
             $table->foreignId('responsable_id')->constrained('users')->onDelete('cascade');
-            $table->foreignIdFor('user_id')->nullable()->constrained()->onDelete('cascade');
-            $table->foreignIdFor('resume_id')->nullable()->constrained()->onDelete('cascade');
-            $table->foreignIdFor('post_id')->nullable()->constrained()->nullOnDelete();
-            $table->foreignIdFor('template_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('resume_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('post_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('template_id')->nullable()->constrained()->nullOnDelete();
             $table->dateTime('date')->nullable();
             $table->integer('type');
-            $table->foreignIdFor('company_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('company_id')->nullable()->constrained()->nullOnDelete();
             $table->integer('decision')->default(1);
             $table->timestamps();
         });
