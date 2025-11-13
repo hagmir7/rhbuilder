de18ad061bf5dd0f5744df750fabdfed112833bf
Agmir Hassane <hagmir6@gmail.com>
2025-11-13 08:44:19 +0100

    modified:   app/Http/Controllers/CalendarController.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        DB::statement("SET DATEFORMAT ymd");
+        DB::statement("SET LANGUAGE English");
     }
 }
